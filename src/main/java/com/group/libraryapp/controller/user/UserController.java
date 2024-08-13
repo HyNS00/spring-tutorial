5e8425f79a86d4db04082be80062f8be4813d4c5
Hyso <hsk7953@gmail.com>
2024-08-13 21:14:56 +0900

    get api와 post api도 변경

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -5,42 +5,28 @@ import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.service.user.UserService;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 public class UserController {
     // private final List<User> users = new ArrayList<>();
     // 유저를 필드로 관리하던 것을 db와 연결해서 사용
-    private final JdbcTemplate jdbcTemplate;
+
     private final UserService userService;
     public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
         this.userService = new UserService(jdbcTemplate);
     }
 
     @PostMapping("/user") //POST/user
     public void saveUser(@RequestBody UserCreateRequest request){
-        String sql ="INSERT INTO user (name,age) VALUES (?,?)";
-        jdbcTemplate.update(sql,request.getName(),request.getAge());
+        userService.saveUser(request);
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers(){
-        String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql,new RowMapper<UserResponse>(){
-            @Override
-            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                int age = rs.getInt("age");
-                return new UserResponse(id,name,age);
-            }
-        });
+        return userService.getUsers();
     }
 
     @PutMapping("/user")
--- src/main/java/com/group/libraryapp/repository/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -1,7 +1,13 @@
 package com.group.libraryapp.repository.user;
 
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 public class UserRepository {
     private final JdbcTemplate jdbcTemplate;
@@ -29,5 +35,22 @@ public class UserRepository {
     public void deleteUserNow(String name){
         String sql = "DELETE FROM user WHERE name = ?";
         jdbcTemplate.update(sql,name);
+    }
+    public void saveUser(String name, Integer age){
+        String sql ="INSERT INTO user (name,age) VALUES (?,?)";
+        jdbcTemplate.update(sql,name,age);
+    }
+
+    public List<UserResponse> getUsers(){
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql,new RowMapper<UserResponse>(){
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                return new UserResponse(id,name,age);
+            }
+        });
     }
 }
