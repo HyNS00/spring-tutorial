8ac18bc71d3a0cb8a3a3085cf26872c15a64eb78
Hyso <hsk7953@gmail.com>
2024-08-16 21:40:18 +0900

    @Transactional를 붙였을 때

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -19,12 +19,13 @@ public class UserServiceV2 {
     public UserServiceV2(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
+    @Transactional // 아래에 있는 함수에 대해 트랜잭션을 적용하게 도와줌
+    // 따라서 문제가 없다면 commit, 문제가 생길시 rollback
     public void saveUser(UserCreateRequest request){
         userRepository.save(new User(request.getName(), request.getAge()));
         // save()에 객체를 넣어주면 INSERT가 자동으로 날라감
     }
-
+    @Transactional
     public List<UserResponse> getUsers() {
         List<User> users = userRepository.findAll(); // 모든 데이터를 가져온다
         return users.stream().
@@ -32,13 +33,14 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
 
     }
+    @Transactional
     public void updateUser(UserUpdateRequest request){
        User user =  userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
        user.updateName(request.getName());
        userRepository.save(user);
     }
-
+    @Transactional
     public void deleteUser(String name){
        User user =  userRepository.findByName(name);
        // 반환값이 user니까 user가 없다면 null호출
