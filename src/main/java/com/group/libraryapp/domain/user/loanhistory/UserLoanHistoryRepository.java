ac98a11131a02131d64aa4679867a443745f7625
Hyso <hsk7953@gmail.com>
2024-08-18 15:32:29 +0900

    책 대출을 위한 entity & repository 생성

---
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.java
@@ -0,0 +1,8 @@
+package com.group.libraryapp.domain.user.loanhistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLoanHistoryRepository extends JpaRepository<UserLoanHistory,Long> {
+    // SELECT * FROM user_loan_history where book_name = ? and is_return = ?;
+    boolean existsByBookNameAndIsReturn(String bookName, boolean isReturn);
+}
