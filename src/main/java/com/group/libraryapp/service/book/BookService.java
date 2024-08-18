feaa03047823c46e7efb0ccc4a881da08a119c28
Hyso <hsk7953@gmail.com>
2024-08-18 15:33:39 +0900

    책 대출 기능 추가

--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -3,16 +3,25 @@ package com.group.libraryapp.service.book;
 
 import com.group.libraryapp.domain.book.Book;
 import com.group.libraryapp.domain.book.BookRepository;
+import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.domain.user.UserRepository;
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository;
 import com.group.libraryapp.dto.book.request.BookCreateRequest;
+import com.group.libraryapp.dto.book.request.BookLoanRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookService {
     private final BookRepository bookRepository;
-
-    public BookService(BookRepository bookRepository) {
+    private final UserLoanHistoryRepository userLoanHistoryRepository;
+    private final UserRepository userRepository;
+    public BookService(BookRepository bookRepository, UserLoanHistoryRepository userLoanHistoryRepository,
+                       UserRepository userRepository) {
         this.bookRepository = bookRepository;
+        this.userLoanHistoryRepository = userLoanHistoryRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
@@ -20,4 +29,22 @@ public class BookService {
         bookRepository.save(new Book(request.getName()));
     }
 
+    @Transactional
+    public void loanBook(BookLoanRequest request){
+        // 책의이름을 가지고 존재하는지 확인
+       Book book = bookRepository.findByName(request.getBookName())
+                .orElseThrow(IllegalArgumentException::new);
+
+
+       // 대출 여부를 확인
+       if(userLoanHistoryRepository.existsByBookNameAndIsReturn(book.getName(),false)){
+           throw new IllegalArgumentException("대출되어 있는 책입니다.");
+       }
+       // 유저정보를 가져온다.
+        User user = userRepository.findByName(request.getUserName())
+                .orElseThrow(IllegalArgumentException::new);
+
+       // 유저정보와 책 정보를 기반으로 UserLoanHistory를 저장
+        userLoanHistoryRepository.save(new UserLoanHistory(user.getId(), book.getName(), false));
+    }
 }
